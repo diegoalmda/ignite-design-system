c65e50d7353a9d7c18201657493dfaa01a43513b
Diego Almeida <diegoalmda@gmail.com>
2023-01-04 17:41:31 -0300

    MultiStep component created

--- packages/docs/src/stories/Avatar.stories.tsx
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -8,6 +8,13 @@ export default {
     src: 'https://github.com/diegoalmda.png',
     alt: 'Diego Almeida',
   },
+  argTypes: {
+    src: {
+      control: {
+        type: 'text',
+      },
+    },
+  },
 } as Meta<AvatarProps>
 
 export const Primary: StoryObj<AvatarProps> = {}
--- packages/docs/src/stories/Text.stories.tsx
+++ packages/docs/src/stories/Text.stories.tsx
@@ -5,8 +5,31 @@ export default {
   title: 'Typography/Text',
   component: Text,
   args: {
+    size: 'md',
     children:
       'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum veritatis delectus exercitationem distinctio eos itaque quibusdam labore eligendi vitae, reprehenderit dolor eveniet aperiam corporis fuga dolore tempora eius repellat laudantium.',
+  },
+  argTypes: {
+    size: {
+      options: [
+        'xxs',
+        'xs',
+        'sm',
+        'md',
+        'lg',
+        'xl',
+        '2xl',
+        '4xl',
+        '5xl',
+        '6xl',
+        '7xl',
+        '8xl',
+        '9xl',
+      ],
+      control: {
+        type: 'inline-radio',
+      },
+    },
   },
 } as Meta<TextProps>
 
